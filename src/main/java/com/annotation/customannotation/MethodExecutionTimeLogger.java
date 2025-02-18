54b160af195b6cc3d7d6afb13746d74357bb85a8
virajkushwaha <virajkushwaha21@gmail.com>
2025-02-18 12:24:03 +0530

    Viraj Add.Adding file MethodExecutionTimeLogger

--- src/main/java/com/annotation/customannotation/MethodExecutionTimeLogger.java
+++ src/main/java/com/annotation/customannotation/MethodExecutionTimeLogger.java
@@ -1,6 +1,9 @@
-package annotation.customannotation;
+package com.annotation.customannotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Method;
 
 // Define the @LogExecutionTime annotation
